35fbcb2fc5740dbde07a88bd2b291517259018d1
Azarudeen <azarudeen.b2003@gmail.com>
2026-02-03 12:35:34 +0530

    search function completed

--- Assets/scripts/home.js
+++ Assets/scripts/home.js
@@ -3,6 +3,9 @@ var products=[
         id:1,name:"samsung",price:9999,color:"red",image:"./Assets/images/samsung.png"
     },
      {
+        id:1,name:" samsung",price:9999,color:"red",image:"./Assets/images/samsung.png"
+    },
+     {
         id:2,name:"vivo",price:19999,color:"gray",image:"./Assets/images/vivo.png"
     },
      {
@@ -35,3 +38,22 @@ function getData(){
     
 }
  getData()
+
+ function searchFun(e){
+    e.preventDefault()
+    var searchData=document.getElementById("searchInput").value
+    console.log("searchData",searchData);
+    var data=""
+    products.filter((value,index)=>value.name.toLowerCase()==searchData.trim().toLowerCase()).map((value,index)=>(
+        data+= `<div class="col-lg-4">
+            <div class="card">
+                <img src="${value.image}" width="100%" height="200px" alt="">
+                <div class="card-body">
+                    <span>name:${value.name}</span>
+                    <span>price:${value.price}</span>
+                </div>
+            </div>
+        </div>`
+    ))
+    document.getElementById("searchdata").innerHTML=data
+ }
